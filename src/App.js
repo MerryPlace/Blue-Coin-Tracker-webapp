015ed6720f443480b3ac3ecde5439e502c200b73
merry.place <merry.professional@gmail.com>
2025-02-28 14:32:35 -0500

    Persistent Settings

--- src/App.js
+++ src/App.js
@@ -9,12 +9,49 @@ import "./App.scss";
 import { DARK_COLORS, LIGHT_COLORS } from "./_constants";
 import SettingsPopup from "./Components/SettingsPopup/SettingsPopup";
 
+const COLOR_THEME_KEY = "BC_COLOR_THEME_KEY";
+const CHECK_THEME_KEY = "BC_CHECK_THEME_KEY";
+const FONT_KEY = "BC_FONT_KEY";
+
 function App() {
-  const [theme, setTheme] = React.useState("dark");
+  const [theme, setTheme] = React.useState(() => {
+    const localData = JSON.parse(localStorage.getItem(COLOR_THEME_KEY));
+    console.log(
+      `Setting theme ${localData === null ? "fresh" : "from storage"}`
+    );
+    return localData === null ? "dark" : localData;
+  });
   const themeColors = theme === "dark" ? DARK_COLORS : LIGHT_COLORS;
-  const [font, setFont] = React.useState("sunshine");
-  const [themeCheckbox, setThemeCheckbox] = React.useState(true);
+
+  const [font, setFont] = React.useState(() => {
+    const localData = JSON.parse(localStorage.getItem(FONT_KEY));
+    console.log(
+      `Setting font setting ${localData === null ? "fresh" : "from storage"}`
+    );
+    return localData === null ? "sunshine" : localData;
+  });
+
+  const [checkboxTheme, setThemeCheckbox] = React.useState(() => {
+    const localData = JSON.parse(localStorage.getItem(CHECK_THEME_KEY));
+    console.log(
+      `Setting checkbox theme ${localData === null ? "fresh" : "from storage"}`
+    );
+    return localData === null ? true : localData;
+  });
+
   const [settingsVisible, setSettingsVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    if (checkboxTheme !== undefined) {
+      localStorage.setItem(CHECK_THEME_KEY, JSON.stringify(checkboxTheme));
+    }
+    if (font !== undefined) {
+      localStorage.setItem(FONT_KEY, JSON.stringify(font));
+    }
+    if (theme !== undefined) {
+      localStorage.setItem(COLOR_THEME_KEY, JSON.stringify(theme));
+    }
+  }, [theme, font, checkboxTheme]);
 
   return (
     <div
@@ -32,9 +69,9 @@ function App() {
               const newFont = font === "sunshine" ? "default" : "sunshine";
               setFont(newFont);
             }}
-            checkboxCheck={themeCheckbox}
+            checkboxCheck={checkboxTheme}
             checkboxToggle={() => {
-              setThemeCheckbox(!themeCheckbox);
+              setThemeCheckbox(!checkboxTheme);
             }}
             themeCheck={theme === "dark"}
             themeToggle={() => {
@@ -52,7 +89,7 @@ function App() {
           />
           <div
             className={`pageWrapper ${
-              themeCheckbox && bcStyles["custom-checkboxes"]
+              checkboxTheme && bcStyles["custom-checkboxes"]
             }`}
           >
             <LevelList levels={levels} />
